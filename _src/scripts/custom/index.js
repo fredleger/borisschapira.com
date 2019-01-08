fcf9d9fced92debb1c8738d2a8ee56c0cd5a72ff
Boris SCHAPIRA <borisschapira@gmail.com>
2019-01-08 10:07:02 +0100

    Refonte du thème

--- _src/scripts/custom/index.js
+++ _src/scripts/custom/index.js
@@ -34,6 +34,11 @@ function perfmark(callback, key) {
 })();
 
 ready(function () {
+
+    perfmark(function () {
+      window.initEasyToggleState();
+    }, 'easy_toggle');
+
     (function (abbrTouch) {
 
         var tooltipTimeout;
@@ -155,4 +160,8 @@ ready(function () {
             }
         }
     })();
+
+    perfmark(function () {
+        window.hljs.initHighlighting();
+    }, 'highlightjs');
 });
